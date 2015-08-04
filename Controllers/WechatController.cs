b50ab3f97ad700320ebd6eb2b1d75a3a298a5f0e
frllk <1402805300@qq.com>
2015-08-04 17:53:57 +0800

    被动回复信息
    被动回复信息

--- Controllers/WechatController.cs
+++ Controllers/WechatController.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using smfc.wechat.com.wxapi;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 using System.Web.Security;
 using System.Xml;
 
@@ -12,36 +14,86 @@ namespace smfc.wechat.com.Controllers
 {
     public class WechatController : Controller
     {
-
         //
         // GET: /Wechat/
-
         public ActionResult Index()
         {
             Log();
-            if (!isAuthorty()) return null;
-            if (Request.HttpMethod != "GET")
+            var MenuJson = @"{'button':[
+     {	
+          'type':'click',
+          'name':'今日歌曲',
+          'key':'V1001_TODAY_MUSIC'
+      },
+      {
+           'type':'click',
+           'name':'歌手简介',
+           'key':'V1001_TODAY_SINGER'
+      },
+      {
+           'name':'菜单',
+           'sub_button':[
+           {	
+               'type':'view',
+               'name':'搜索',
+               'url':'http://www.soso.com/'
+            },
             {
-                using (Stream input = Request.InputStream)
-                {
-                    input.Position = 0;
-                    using (StreamReader reader = new StreamReader(input))
-                    {
-                        System.IO.File.AppendAllText(Server.MapPath("~/Logs/info.html"), reader.ReadToEnd());
-                    }
-                }
-                return Content(@"<xml>
-                    <ToUserName><![CDATA[CDATA[oS_iCvwyarJMkpB-a-WPASqxXz3g]]></ToUserName>
-                    <FromUserName><![CDATA[gh_bdf6a2107fdc]]></FromUserName>
-                    <CreateTime>1438523413</CreateTime>
-                    <MsgType><![CDATA[text]]></MsgType>
-                    <Content><![CDATA[你好]]></Content>
-                    </xml>");
-            }
-            else
+               'type':'view',
+               'name':'视频',
+               'url':'http://v.qq.com/'
+            },
             {
-                return Content(Request["echostr"]);
-            }
+               'type':'click',
+               'name':'赞一下我们',
+               'key':'V1001_GOOD'
+            }]
+       }]
+ }".Replace("\r\n", "").Replace("\t", "").Replace(" ", "").Replace("'", "\"");
+            JavaScriptSerializer Jss = new JavaScriptSerializer();
+            string setMenuUrl = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token={0}";
+            setMenuUrl = string.Format(setMenuUrl, "wwwfrllkcom ");//获取token、拼凑url
+            string respText = common.CommonMethod.WebRequestPostOrGet(setMenuUrl, MenuJson);
+            Dictionary<string, object> respDic = (Dictionary<string, object>)Jss.DeserializeObject(respText);
+            return Content(respDic["errcode"].ToString());//返回0发布成功
+            #region 微信 验证  微信发送信息  被动回复信息
+            //            if (Request.HttpMethod != "GET" && isAuthorty())
+            //            {
+            //                return Content(@"<xml><ToUserName><![CDATA[oS_iCvwyarJMkpB-a-WPASqxXz3g]]></ToUserName>
+            //            <FromUserName><![CDATA[gh_bdf6a2107fdc]]></FromUserName><CreateTime>" + ConvertDateTimeInt(DateTime.Now) + "</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[有]]></Content><MsgId>6179046501229613247</MsgId></xml>");
+            //                using (Stream input = Request.InputStream)
+            //                {
+            //                    input.Position = 0;
+            //                    using (StreamReader reader = new StreamReader(input))
+            //                    {
+            //                        System.IO.File.AppendAllText(Server.MapPath("~/Logs/info.html"), reader.ReadToEnd());
+            //                    }
+            //                }
+            //                return Content(@"<xml><ToUserName><![CDATA[oS_iCvwyarJMkpB-a-WPASqxXz3g]]></ToUserName>
+            //            <FromUserName><![CDATA[gh_bdf6a2107fdc]]></FromUserName><CreateTime>" + ConvertDateTimeInt(DateTime.Now) + "</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[有]]></Content><MsgId>6179046501229613247</MsgId></xml>");
+            //            }
+            //            else
+            //            {
+            //                if (!isAuthorty())
+            //                {
+            //                    //return null;
+            //                    return Content("error：verification failed");
+            //                }
+            //                return Content(Request["echostr"]);
+            //            }
+            #endregion
+        }
+        /// <summary>
+        /// 将c# DateTime时间格式转换为Unix时间戳格式
+        /// </summary>
+        /// <param name="time">时间</param>
+        /// <returns>double</returns>
+        public static double ConvertDateTimeInt(System.DateTime time)
+        {
+            double intResult = 0;
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+            intResult = (time - startTime).TotalSeconds;
+            return intResult;
         }
         public bool isAuthorty()
         {
@@ -67,6 +119,10 @@ namespace smfc.wechat.com.Controllers
             string file = Server.MapPath("~/Logs/access.txt");
             return Content(System.IO.File.ReadAllText(file));
         }
-
+        public ActionResult ViewInfo()
+        {
+            string file = Server.MapPath("~/Logs/info.html");
+            return Content(System.IO.File.ReadAllText(file));
+        }
     }
 }
